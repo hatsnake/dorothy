7a4068b978dd4fb7eeeff8dae8f342f6b1611808
hatsnake <60315837+hatsnake@users.noreply.github.com>
2023-09-12 01:11:50 +0900

    init
    init

--- src/main/java/com/ucamp/dorothy/mapper/BoardMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ucamp.dorothy.mapper;
-
-import java.util.List;
-
-import com.ucamp.dorothy.domain.Board;
-
-public interface BoardMapper {
-	public List<Board> list() throws Exception;
-}
--- src/main/java/com/ucamp/dorothy/service/BoardService.java
+++
@@ -1,9 +0,0 @@
-package com.ucamp.dorothy.service;
-
-import java.util.List;
-
-import com.ucamp.dorothy.domain.Board;
-
-public interface BoardService {
-	public List<Board> list() throws Exception;
-}
