b71115a5fd40a4f7d1d672265d1624c18748e305
hatsnake <60315837+hatsnake@users.noreply.github.com>
2023-11-25 00:37:38 +0900

    일반 로그인할 경우 id, email, name 세션 저장 및 조회
    일반 로그인할 경우 id, email, name 세션 저장 및 조회

--- src/main/java/com/ucamp/dorothy/common/security/CustomLoginSuccessHandler.java
+++ src/main/java/com/ucamp/dorothy/common/security/CustomLoginSuccessHandler.java
@@ -5,25 +5,53 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Service;
 
+import com.ucamp.dorothy.common.mail.SendMail;
+import com.ucamp.dorothy.common.sms.SendSms;
+import com.ucamp.dorothy.domain.Member;
+import com.ucamp.dorothy.mapper.MemberMapper;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Service
 public class CustomLoginSuccessHandler extends 
 SavedRequestAwareAuthenticationSuccessHandler{
+	@Autowired
+	private HttpSession httpSession;
+	@Autowired
+	private MemberMapper memberMapper;
 	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication auth) throws ServletException, IOException {
 		log.info("onAuthenticationSuccess");
 		
+		
 		User customUser = (User) auth.getPrincipal();
 		
 		log.info("username = " + customUser.getUsername());
+		
+		try {
+			Member member = memberMapper.findByEmail(customUser.getUsername());
+			
+			if(member != null) {
+				httpSession.setAttribute("id", member.getId());
+				httpSession.setAttribute("email", member.getEmail());
+				httpSession.setAttribute("name", member.getName());
+			}
+		} catch(Exception e) {
+			log.error(e.getMessage());
+		}
 		
 		super.onAuthenticationSuccess(request, response, auth);
 	}
--- src/main/java/com/ucamp/dorothy/controller/MainController.java
+++ src/main/java/com/ucamp/dorothy/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.ucamp.dorothy.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +15,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class MainController {
+	@Autowired
+	private HttpSession httpSession;
 	
 	@GetMapping("/")
 	public String main() {
+		
+	    log.info("멤버 아이디 : " + httpSession.getAttribute("id") + ", 이메일 : " + httpSession.getAttribute("email"));
 		log.info("Main Page Start");
 		log.info("TEST");
 		log.info("Main Page End");
